d1ce850f19b9f38fb706f3c7f69f09df4210e175
BAVANIKA <92446957+V-Bavanika@users.noreply.github.com>
2023-02-17 16:21:37 +0530

    initial commit

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+//File: sample.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I'm Bavanika - PES2UG20CS374\n";
+  cout << "New C++ program for task5\n";
+  return 0;
+}
